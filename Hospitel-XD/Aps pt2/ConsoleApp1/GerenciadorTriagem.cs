56556efe90a47c5ec6408a359afdf0379dbd580b
1Dereck <pedrequewr@gmail.com>
2025-06-12 20:18:17 -0300

    Update GerenciadorTriagem.cs

--- Hospitel-XD/Aps pt2/ConsoleApp1/GerenciadorTriagem.cs
+++ Hospitel-XD/Aps pt2/ConsoleApp1/GerenciadorTriagem.cs
@@ -1,27 +1,31 @@
-﻿public class GerenciadorTriagem
+﻿namespace ProjetoHospital
 {
-    private Queue<Paciente> filaTriagem = new();
-
-    public void AdicionarPaciente(Paciente paciente)
+    public class GerenciadorTriagem
     {
-        filaTriagem.Enqueue(paciente);
-    }
+        private Queue<Paciente> filaTriagem = new();
 
-    public Paciente AtenderPaciente()
-    {
-        if (filaTriagem.Count == 0)
-            return null;
+        public void AdicionarPaciente(Paciente paciente)
+        {
+            filaTriagem.Enqueue(paciente);
+        }
 
-        var paciente = filaTriagem.Dequeue();
+        public Paciente AtenderPaciente()
+        {
+            if (filaTriagem.Count == 0)
+                return null;
 
-        // Lógica de triagem
-        if (paciente.Temperatura >= 39 || paciente.Oxigenacao <= 92)
-            paciente.Prioridade = "Vermelho";
-        else if (paciente.Temperatura >= 37.5 || paciente.PressaoArterial >= 14)
-            paciente.Prioridade = "Amarelo";
-        else
-            paciente.Prioridade = "Verde";
+            var paciente = filaTriagem.Dequeue();
 
-        return paciente;
+            // Lógica de triagem
+            if (paciente.Temperatura >= 39 || paciente.Oxigenacao <= 92)
+                paciente.Prioridade = "Vermelho";
+            else if (paciente.Temperatura >= 37.5 || paciente.PressaoArterial >= 14)
+                paciente.Prioridade = "Amarelo";
+            else
+                paciente.Prioridade = "Verde";
+
+            return paciente;
+        }
     }
 }
+
