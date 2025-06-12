95ef64524b7ee01661ae3bfa71dbaf1b5741f3be
1Dereck <pedrequewr@gmail.com>
2025-06-12 20:05:08 -0300

    Update Paciente.cs
    Eu esqueci de colocar um namespace ProjetoHospital

--- Hospitel-XD/Aps pt2/ConsoleApp1/Paciente.cs
+++ Hospitel-XD/Aps pt2/ConsoleApp1/Paciente.cs
@@ -1,34 +1,37 @@
-﻿public class Paciente
+namespace ProjetoHospital
 {
-    public string Nome { get; set; }
-    public int Idade { get; set; }
-    public double PressaoArterial { get; set; }
-    public double Temperatura { get; set; }
-    public int Oxigenacao { get; set; }
-    public string Prioridade { get; set; }
-
-    public Paciente(string nome, int idade, double pressao, double temperatura, int oxigenacao)
+    public class Paciente
     {
-        Nome = nome;
-        Idade = idade;
-        PressaoArterial = pressao;
-        Temperatura = temperatura;
-        Oxigenacao = oxigenacao;
-        Prioridade = DefinirPrioridade();
-    }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double PressaoArterial { get; set; }
+        public double Temperatura { get; set; }
+        public int Oxigenacao { get; set; }
+        public string Prioridade { get; set; }
 
-    private string DefinirPrioridade()
-    {
-        if (Oxigenacao < 90 || Temperatura >= 39 || PressaoArterial < 9)
-            return "Vermelho";
-        else if ((Oxigenacao >= 90 && Oxigenacao <= 94) || (Temperatura > 37.5 && Temperatura < 39))
-            return "Amarelo";
-        else
-            return "Verde";
-    }
+        public Paciente(string nome, int idade, double pressao, double temperatura, int oxigenacao)
+        {
+            Nome = nome;
+            Idade = idade;
+            PressaoArterial = pressao;
+            Temperatura = temperatura;
+            Oxigenacao = oxigenacao;
+            Prioridade = DefinirPrioridade();
+        }
 
-    public override string ToString()
-    {
-        return $"Nome: {Nome} | Idade: {Idade} | PA: {PressaoArterial} | Temp: {Temperatura} | O2: {Oxigenacao} | Prioridade: {Prioridade}";
+        private string DefinirPrioridade()
+        {
+            if (Oxigenacao < 90 || Temperatura >= 39 || PressaoArterial < 9)
+                return "Vermelho";
+            else if ((Oxigenacao >= 90 && Oxigenacao <= 94) || (Temperatura > 37.5 && Temperatura < 39))
+                return "Amarelo";
+            else
+                return "Verde";
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} | Idade: {Idade} | PA: {PressaoArterial} | Temp: {Temperatura} | O2: {Oxigenacao} | Prioridade: {Prioridade}";
+        }
     }
 }
